4185fb9b83b243e93a2bbccfa826e7acb56d8ad7
Max Huang <risonhuang@gmail.com>
2016-07-07 23:26:23 +1000

    Improve demos

--- js/main.js
+++ js/main.js
@@ -82,7 +82,7 @@ selectNumeral.addEventListener('change', function () {
 // delimiter
 var cleaveDelimeter = new Cleave('.input-delimiter', {
     delimiter:  '·',
-    creditCard: true
+    blocks: [3, 3, 3]
 });
 
 // blocks
