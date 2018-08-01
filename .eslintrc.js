fb747106039512e816945e9a2017bd4907b0be1c
Max Huang <risonhuang@gmail.com>
2018-08-01 22:18:51 +1000

    Format config files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,27 @@
 module.exports = {
-    "parser":       "babel-eslint",
-    "globals":      {
-        "angular": 1
-    },
-    "rules":        {
-        "quotes":          [
-            2,
-            "single"
-        ],
-        "linebreak-style": [
-            2,
-            "unix"
-        ],
-        "semi":            [
-            2,
-            "always"
-        ]
-    },
-    "env":          {
-        "node":     true,
-        "browser":  true,
-        "es6":      true,
-        "commonjs": true,
-        "amd":      true,
-        "jest":     true,
-        "mocha":    true,
-        "jquery":   true
-    },
-    "extends":      "eslint:recommended",
-    "ecmaFeatures": {
-        "jsx":                          true,
-        "experimentalObjectRestSpread": true
-    },
-    "plugins":      [
-        "react"
-    ]
+  parser: 'babel-eslint',
+  globals: {
+    angular: 1
+  },
+  rules: {
+    quotes: [2, 'single'],
+    'linebreak-style': [2, 'unix'],
+    semi: [2, 'always']
+  },
+  env: {
+    node: true,
+    browser: true,
+    es6: true,
+    commonjs: true,
+    amd: true,
+    jest: true,
+    mocha: true,
+    jquery: true
+  },
+  extends: 'eslint:recommended',
+  ecmaFeatures: {
+    jsx: true,
+    experimentalObjectRestSpread: true
+  },
+  plugins: ['react']
 };
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,3 @@
 var requireDir = require('require-dir');
 
 requireDir('./gulp-tasks');
-
-
