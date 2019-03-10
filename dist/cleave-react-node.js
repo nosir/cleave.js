4afa2ceac5d3095445e61e0bdf7680a5c6e567eb
Max Huang <risonhuang@gmail.com>
2019-03-10 22:18:04 +1100

    [ISSUE-247][ISSUE-442] Fix react component phone prefix issue

--- dist/cleave-react-node.js
+++ dist/cleave-react-node.js
@@ -355,7 +355,11 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	        // phone formatter
 	        if (pps.phone) {
-	            pps.result = pps.phoneFormatter.format(value);
+	            if (pps.prefix && (!pps.noImmediatePrefix || value.length)) {
+	                pps.result = pps.prefix + pps.phoneFormatter.format(value).slice(pps.prefix.length);
+	            } else {
+	                pps.result = pps.phoneFormatter.format(value);
+	            }
 	            owner.updateValueState();
 
 	            return;
