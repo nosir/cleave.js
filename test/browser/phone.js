195aa03c8d06dd7d7694c84ca000de7f42f3cabe
Nipun Paradkar <nipunparadkar123@gmail.com>
2019-12-01 21:26:30 +0530

    Add phone tests

--- test/browser/phone.js
+++ test/browser/phone.js
@@ -1,33 +1,70 @@
 describe('Phone number input field', function () {
     var field = document.querySelector('.input-phone');
-    var cleave = new Cleave(field, {
-        phone:           true,
-        phoneRegionCode: 'AU'
-    });
 
-    it('should format fully matched input value', function () {
-        cleave.setRawValue('0416123456');
-        assert.equal(field.value, '0416 123 456');
-    });
-
-    it('should format partially matched input value', function () {
-        cleave.setRawValue('0416123');
-        assert.equal(field.value, '0416 123');
-    });
-
-    it('should format input value with country code', function () {
-        cleave.setRawValue('+61416123');
-        assert.equal(field.value, '+61 416 123');
-    });
-
-    it('should use defined prefix', function () {
-        var cleave2 = new Cleave(field, {
-            prefix:    '+61',
+    describe('on initialization', function () {
+        var cleave = new Cleave(field, {
             phone:           true,
             phoneRegionCode: 'AU'
         });
+    
+        it('should format fully matched input value', function () {
+            cleave.setRawValue('0416123456');
+            assert.equal(field.value, '0416 123 456');
+        });
+    
+        it('should format partially matched input value', function () {
+            cleave.setRawValue('0416123');
+            assert.equal(field.value, '0416 123');
+        });
+    
+        it('should format input value with country code', function () {
+            cleave.setRawValue('+61416123');
+            assert.equal(field.value, '+61 416 123');
+        });
+    
+        it('should use defined prefix', function () {
+            var cleave2 = new Cleave(field, {
+                prefix:    '+61',
+                phone:           true,
+                phoneRegionCode: 'AU'
+            });
+    
+            cleave2.setRawValue('+61416123456');
+            assert.equal(field.value, '+61 416 123 456');
+        });
+    });
 
-        cleave2.setRawValue('+61416123456');
-        assert.equal(field.value, '+61 416 123 456');
+    describe('on modification', function () {
+        var cleave = new Cleave(field);
+        cleave.modify({
+            phone:           true,
+            phoneRegionCode: 'AU'
+        });
+    
+        it('should format fully matched input value', function () {
+            cleave.setRawValue('0416123456');
+            assert.equal(field.value, '0416 123 456');
+        });
+    
+        it('should format partially matched input value', function () {
+            cleave.setRawValue('0416123');
+            assert.equal(field.value, '0416 123');
+        });
+    
+        it('should format input value with country code', function () {
+            cleave.setRawValue('+61416123');
+            assert.equal(field.value, '+61 416 123');
+        });
+    
+        it('should use defined prefix', function () {
+            var cleave2 = new Cleave(field, {
+                prefix:    '+61',
+                phone:           true,
+                phoneRegionCode: 'AU'
+            });
+    
+            cleave2.setRawValue('+61416123456');
+            assert.equal(field.value, '+61 416 123 456');
+        });
     });
 });
