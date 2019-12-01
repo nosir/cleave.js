43359c21d1f05462ab64328a688dbc0a381875e8
Nipun Paradkar <nipunparadkar123@gmail.com>
2019-12-01 21:28:35 +0530

    Publish

--- dist/cleave-esm.js
+++ dist/cleave-esm.js
@@ -1033,6 +1033,83 @@ var DefaultProperties = {
 var DefaultProperties_1 = DefaultProperties;
 
 /**
+ * Props override
+ */
+var ExistingProperties = {
+    assign: function (target, opts) {
+        target = target || {};
+        opts = opts || {};
+
+        // credit card
+        target.creditCard = opts.creditCard === undefined ? target.creditCard : opts.creditCard;
+        target.creditCardStrictMode = opts.creditCardStrictMode === undefined ? target.creditCardStrictMode : opts.creditCardStrictMode;
+        target.onCreditCardTypeChanged = opts.onCreditCardTypeChanged || target.onCreditCardTypeChanged;
+
+        // phone
+        target.phone = opts.phone === undefined ? target.phone : opts.phone;
+        target.phoneRegionCode = opts.phoneRegionCode || target.phoneRegionCode;
+
+        // time
+        target.time = opts.time === undefined ? target.time : opts.time;
+        target.timePattern = opts.timePattern || target.timePattern;
+        target.timeFormat = opts.timeFormat || target.timeFormat;
+
+        // date
+        target.date = opts.date === undefined ? target.date : opts.date;
+        target.datePattern = opts.datePattern || target.datePattern;
+        target.dateMin = opts.dateMin || target.dateMin;
+        target.dateMax = opts.dateMax || target.dateMax;
+
+        // numeral
+        target.numeral = opts.numeral === undefined ? target.numeral : opts.numeral;
+        target.numeralIntegerScale = opts.numeralIntegerScale > 0 ? opts.numeralIntegerScale : target.numeralIntegerScale;
+        target.numeralDecimalScale = opts.numeralDecimalScale >= 0 ? opts.numeralDecimalScale : target.numeralDecimalScale;
+        target.numeralDecimalMark = opts.numeralDecimalMark || target.numeralDecimalMark;
+        target.numeralThousandsGroupStyle = opts.numeralThousandsGroupStyle || target.numeralThousandsGroupStyle;
+        target.numeralPositiveOnly = opts.numeralPositiveOnly === undefined ? target.numeralPositiveOnly : opts.numeralPositiveOnly;
+        target.stripLeadingZeroes = opts.stripLeadingZeroes === undefined ? target.stripLeadingZeroes : opts.stripLeadingZeroes;
+        target.signBeforePrefix = opts.signBeforePrefix || target.signBeforePrefix;
+
+        // others
+        target.numericOnly = target.creditCard || target.date || !!opts.numericOnly;
+
+        target.uppercase = opts.uppercase === undefined ? target.uppercase : opts.uppercase;
+        target.lowercase = opts.lowercase === undefined ? target.lowercase : opts.lowercase;
+
+        target.prefix = opts.prefix || target.prefix;
+        target.noImmediatePrefix = opts.noImmediatePrefix === undefined ? target.noImmediatePrefix : opts.noImmediatePrefix;
+        target.prefixLength = (opts.prefix || target.prefix).length;
+        target.rawValueTrimPrefix = opts.rawValueTrimPrefix === undefined ? target.rawValueTrimPrefix : opts.rawValueTrimPrefix;
+        target.copyDelimiter = opts.copyDelimiter === undefined ? target.copyDelimiter : opts.copyDelimiter;
+
+        target.initValue = (opts.initValue !== undefined && opts.initValue !== null) ? opts.initValue.toString() : target.initValue;
+
+        target.delimiter =
+            (opts.delimiter || opts.delimiter === '') ? opts.delimiter :
+                (opts.date ? '/' :
+                    (opts.time ? ':' :
+                        (opts.numeral ? ',' :
+                            (opts.phone ? ' ' :
+                                target.delimiter))));
+        target.delimiterLength = target.delimiter.length;
+        target.delimiterLazyShow = opts.delimiterLazyShow === undefined ? target.delimiterLazyShow : opts.delimiterLazyShow;
+        target.delimiters = opts.delimiters || target.delimiters;
+
+        target.blocks = opts.blocks || target.blocks;
+        target.blocksLength = target.blocks.length;
+
+        target.root = (typeof commonjsGlobal === 'object' && commonjsGlobal) ? commonjsGlobal : window;
+        target.document = opts.document || target.document;
+
+        target.onValueChanged = opts.onValueChanged || target.onValueChanged;
+
+        return target;
+    }
+};
+
+var ExistingProperties_1 = ExistingProperties;
+
+/**
  * Construct a new Cleave instance by passing the configuration object
  *
  * @param {String | HTMLElement} element
@@ -1041,6 +1118,7 @@ var DefaultProperties_1 = DefaultProperties;
 var Cleave = function (element, opts) {
     var owner = this;
     var hasMultipleElements = false;
+    opts = opts || {};
 
     if (typeof element === 'string') {
         owner.element = document.querySelector(element);
@@ -1479,6 +1557,11 @@ Cleave.prototype = {
         return this.element.value;
     },
 
+    modify: function (opts) {
+        Cleave.ExistingProperties.assign(this.properties, opts);
+        this.init();
+    },
+
     destroy: function () {
         var owner = this;
 
@@ -1501,6 +1584,7 @@ Cleave.PhoneFormatter = PhoneFormatter_1;
 Cleave.CreditCardDetector = CreditCardDetector_1;
 Cleave.Util = Util_1;
 Cleave.DefaultProperties = DefaultProperties_1;
+Cleave.ExistingProperties = ExistingProperties_1;
 
 // for angular directive
 ((typeof commonjsGlobal === 'object' && commonjsGlobal) ? commonjsGlobal : window)['Cleave'] = Cleave;
--- dist/cleave.js
+++ dist/cleave.js
@@ -65,6 +65,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	var Cleave = function (element, opts) {
 	    var owner = this;
 	    var hasMultipleElements = false;
+	    opts = opts || {};
 
 	    if (typeof element === 'string') {
 	        owner.element = document.querySelector(element);
@@ -503,6 +504,11 @@ return /******/ (function(modules) { // webpackBootstrap
 	        return this.element.value;
 	    },
 
+	    modify: function (opts) {
+	        Cleave.ExistingProperties.assign(this.properties, opts);
+	        this.init();
+	    },
+
 	    destroy: function () {
 	        var owner = this;
 
@@ -525,6 +531,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	Cleave.CreditCardDetector = __webpack_require__(5);
 	Cleave.Util = __webpack_require__(6);
 	Cleave.DefaultProperties = __webpack_require__(7);
+	Cleave.ExistingProperties = __webpack_require__(8);
 
 	// for angular directive
 	((typeof global === 'object' && global) ? global : window)['Cleave'] = Cleave;
@@ -1614,6 +1621,88 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	module.exports = DefaultProperties;
 
+	/* WEBPACK VAR INJECTION */}.call(exports, (function() { return this; }())))
+
+/***/ }),
+/* 8 */
+/***/ (function(module, exports) {
+
+	/* WEBPACK VAR INJECTION */(function(global) {/**
+	 * Props override
+	 */
+	var ExistingProperties = {
+	    assign: function (target, opts) {
+	        target = target || {};
+	        opts = opts || {};
+
+	        // credit card
+	        target.creditCard = opts.creditCard === undefined ? target.creditCard : opts.creditCard;
+	        target.creditCardStrictMode = opts.creditCardStrictMode === undefined ? target.creditCardStrictMode : opts.creditCardStrictMode;
+	        target.onCreditCardTypeChanged = opts.onCreditCardTypeChanged || target.onCreditCardTypeChanged;
+
+	        // phone
+	        target.phone = opts.phone === undefined ? target.phone : opts.phone;
+	        target.phoneRegionCode = opts.phoneRegionCode || target.phoneRegionCode;
+
+	        // time
+	        target.time = opts.time === undefined ? target.time : opts.time;
+	        target.timePattern = opts.timePattern || target.timePattern;
+	        target.timeFormat = opts.timeFormat || target.timeFormat;
+
+	        // date
+	        target.date = opts.date === undefined ? target.date : opts.date;
+	        target.datePattern = opts.datePattern || target.datePattern;
+	        target.dateMin = opts.dateMin || target.dateMin;
+	        target.dateMax = opts.dateMax || target.dateMax;
+
+	        // numeral
+	        target.numeral = opts.numeral === undefined ? target.numeral : opts.numeral;
+	        target.numeralIntegerScale = opts.numeralIntegerScale > 0 ? opts.numeralIntegerScale : target.numeralIntegerScale;
+	        target.numeralDecimalScale = opts.numeralDecimalScale >= 0 ? opts.numeralDecimalScale : target.numeralDecimalScale;
+	        target.numeralDecimalMark = opts.numeralDecimalMark || target.numeralDecimalMark;
+	        target.numeralThousandsGroupStyle = opts.numeralThousandsGroupStyle || target.numeralThousandsGroupStyle;
+	        target.numeralPositiveOnly = opts.numeralPositiveOnly === undefined ? target.numeralPositiveOnly : opts.numeralPositiveOnly;
+	        target.stripLeadingZeroes = opts.stripLeadingZeroes === undefined ? target.stripLeadingZeroes : opts.stripLeadingZeroes;
+	        target.signBeforePrefix = opts.signBeforePrefix || target.signBeforePrefix;
+
+	        // others
+	        target.numericOnly = target.creditCard || target.date || !!opts.numericOnly;
+
+	        target.uppercase = opts.uppercase === undefined ? target.uppercase : opts.uppercase;
+	        target.lowercase = opts.lowercase === undefined ? target.lowercase : opts.lowercase;
+
+	        target.prefix = opts.prefix || target.prefix;
+	        target.noImmediatePrefix = opts.noImmediatePrefix === undefined ? target.noImmediatePrefix : opts.noImmediatePrefix;
+	        target.prefixLength = (opts.prefix || target.prefix).length;
+	        target.rawValueTrimPrefix = opts.rawValueTrimPrefix === undefined ? target.rawValueTrimPrefix : opts.rawValueTrimPrefix;
+	        target.copyDelimiter = opts.copyDelimiter === undefined ? target.copyDelimiter : opts.copyDelimiter;
+
+	        target.initValue = (opts.initValue !== undefined && opts.initValue !== null) ? opts.initValue.toString() : target.initValue;
+
+	        target.delimiter =
+	            (opts.delimiter || opts.delimiter === '') ? opts.delimiter :
+	                (opts.date ? '/' :
+	                    (opts.time ? ':' :
+	                        (opts.numeral ? ',' :
+	                            (opts.phone ? ' ' :
+	                                target.delimiter))));
+	        target.delimiterLength = target.delimiter.length;
+	        target.delimiterLazyShow = opts.delimiterLazyShow === undefined ? target.delimiterLazyShow : opts.delimiterLazyShow;
+	        target.delimiters = opts.delimiters || target.delimiters;
+
+	        target.blocks = opts.blocks || target.blocks;
+	        target.blocksLength = target.blocks.length;
+
+	        target.root = (typeof global === 'object' && global) ? global : window;
+	        target.document = opts.document || target.document;
+
+	        target.onValueChanged = opts.onValueChanged || target.onValueChanged;
+
+	        return target;
+	    }
+	};
+
+	module.exports = ExistingProperties;
 	/* WEBPACK VAR INJECTION */}.call(exports, (function() { return this; }())))
 
 /***/ })
